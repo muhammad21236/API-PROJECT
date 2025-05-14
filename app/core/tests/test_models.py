40c42aaa246f492e0f7d601382345cfbe4b62fc5
Muhammad <matta5292@gmail.com>
2025-05-14 05:31:48 +0500

    Implementing tests for tags API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,6 +5,13 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email="user@example.com", password="testpass123"):
+    """Create and return a user with an email and password."""
+    return get_user_model().objects.create_user(
+        email=email, password=password, name="Test User"
+    )
+
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         email = "test@example.com"
@@ -55,3 +62,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name="Tag1")
+
+        self.assertEqual(str(tag), tag.name)
