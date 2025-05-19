0194d9d65c0964751b5497efb2caebe142a2ab63
Muhammad <matta5292@gmail.com>
2025-05-20 02:38:49 +0500

    Tags API [skip ci]

---
+++ app/recipe/serializers.py
@@ -0,0 +1,25 @@
+"""Serializers for recipe APIs."""
+
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipe objects."""
+
+    class Meta:
+        model = Recipe
+        fields = ("id", "title", "description", "time_minutes", "price", "link")
+        read_only_fields = ("id",)
+        extra_kwargs = {
+            "description": {"required": False},
+            "link": {"required": False},
+        }
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ("description", "link")
---
+++ app/recipe/views.py
@@ -0,0 +1,33 @@
+"""Recipe views module."""
+
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from core.models import Recipe
+from recipe import serializers
+# from django.shortcuts import render
+
+# Create your views here.
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for managing recipe APIs."""
+
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by("-id")
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class based on action."""
+        if self.action == "retrieve":
+            return serializers.RecipeSerializer
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
